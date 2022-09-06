361de385f2e3a50e5e42c6cf86bef58142f8ad11
tom <tom.young.316@gmail.com>
2022-09-06 13:34:16 +0100

    Add comment [skip actions]

--- tdms/src/fields/magnetic.cpp
+++ tdms/src/fields/magnetic.cpp
@@ -5,5 +5,5 @@ using namespace std;
 
 
 double MagneticField::phase(int n, double omega, double dt){
-  return omega * ((double) n + 0.5) * dt;
+  return omega * ((double) n + 0.5) * dt;  // 0.5 added because it's known half a time step after E
 }
