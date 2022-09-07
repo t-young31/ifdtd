61d7f34713cb475bc3c55aaa011b02cb50a91236
tom <tom.young.316@gmail.com>
2022-09-07 12:53:17 +0100

    Additional doc [skip actions]

--- tdms/include/simulation_parameters.h
+++ tdms/include/simulation_parameters.h
@@ -1,5 +1,5 @@
 
-struct PML{
+struct PerfectlyMatchedLayer {
   int Dxl = 0;  // Thickness of lower pml in the x direction
   int Dxu = 0;  //              upper
   int Dyl = 0;
@@ -20,7 +20,7 @@ public:
     bool         has_tdfdir    = false;    // Is the tdfdir (time domain field directory) defined?
     bool         is_multilayer = false;    // Is this simulation of a multilayer?
     bool         is_disp_ml    = false;    // Is the material dispersive?
-    double       to_l          = 0.0;      // time delay of pulse
-    double       hwhm          = 0.0;      // hwhm of pulse
-    PML          pml;                      // Perfectly matched layer
+    double       to_l          = 0.0;      // Time delay of pulse
+    double       hwhm          = 0.0;      // Half width at half max of pulse
+    PerfectlyMatchedLayer pml;             // Perfectly matched layer struct with size attributes
 };
